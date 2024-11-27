123f84df0bd114233ac5b2193a3e050f80c869fa
Justin Yu <ajhj3094@eip.chuanhwa.com.tw>
2024-11-27 15:37:33 +0800

    feat: antfo 的 CLI 指令配置 ESlint 自動排版

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,16 @@
-<template>
-    <div>
-        default layout
-        <slot />
-    </div>
-</template>
-
 <script setup lang="ts">
-
+import { } from 'vue'
 </script>
 
-<style scoped></style>
+<template>
+  <div>
+    default layout
+    <slot />
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  text-align: center;
+}
+</style>
