4334586f72104cd2206a609c12e3c3c84a8cf3e4
Justin Yu <ajhj3094@eip.chuanhwa.com.tw>
2024-11-27 15:03:35 +0800

    feat: 新增 layouts/default、app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,5 @@
+export default defineAppConfig({
+    theme: {
+        color: '#0d6efd'
+    }
+})
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        default layout
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
